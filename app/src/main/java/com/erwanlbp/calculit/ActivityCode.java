f1623f422e8561b6500329317c95bf8dcd639b3f
Le Batard--Polès <erwan.lbp@gmail.com>
2017-06-29 19:37:53 +0200

    Print results (#7)
    * Add the activity PrintResults that receive the userAnswer + correctResult and show Correct or Wrong
    Call PrintResultsActivity in the MainActivity

--- app/src/main/java/com/erwanlbp/calculit/ActivityCode.java
+++ app/src/main/java/com/erwanlbp/calculit/ActivityCode.java
@@ -3,6 +3,7 @@ package com.erwanlbp.calculit;
 public class ActivityCode {
     private static int value = 1;
     public final static int MAIN = value++;
+    public final static int SHOW_RESULT= value++;
     public final static int GAME = value++;
     public final static int ANSWER = value++;
 }

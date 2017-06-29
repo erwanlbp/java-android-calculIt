5b72f19fc92801e282996a779df175e96f9f644c
ErwanLBP <erwan.lbp@gmail.com>
2017-06-29 22:26:35 +0200

    add the level in the config add the level on the print result activity nextlevel button enabled

--- app/src/main/java/com/erwanlbp/calculit/activity/PrintResultsActivity.java
+++ app/src/main/java/com/erwanlbp/calculit/activity/PrintResultsActivity.java
@@ -1,17 +1,19 @@
 package com.erwanlbp.calculit.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.erwanlbp.calculit.GameConfig;
 import com.erwanlbp.calculit.R;
 
-import org.w3c.dom.Text;
-
 public class PrintResultsActivity extends AppCompatActivity {
+
+    public static final int NEXT_LEVEL = 2;
+    public static final int BACK_HOME = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,8 @@ public class PrintResultsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         int correctResult = intent.getIntExtra(GameConfig.CONFIG_CORRECT_RESULT, 0);
-        int userAnswer = intent.getIntExtra(AnswerActivity.USER_ANSWER, correctResult - 1);  // To be sure that the default value is wrong
+        int userAnswer = intent.getIntExtra(AnswerActivity.USER_ANSWER, 0);
+        int level = intent.getIntExtra(GameConfig.CONFIG_LEVEL, -1);
 
         TextView tvResultAnswer = (TextView) findViewById(R.id.tvResultAnswer);
         if (correctResult == userAnswer)
@@ -33,9 +36,24 @@ public class PrintResultsActivity extends AppCompatActivity {
 
         TextView tvCorrectResult = (TextView) findViewById(R.id.tvCorrectAnswer);
         tvCorrectResult.setText(String.valueOf(correctResult));
+
+        TextView tvLevel = (TextView) findViewById(R.id.tvPrintResultLevelValue);
+        tvLevel.setText(String.valueOf(level));
+
+        if (correctResult != userAnswer) {
+            Button buttonNextLevel = (Button) findViewById(R.id.button_print_results_next_level);
+            buttonNextLevel.setEnabled(false);
+        }
+
     }
 
     public void backHome(View view) {
+        setResult(BACK_HOME);
+        finish();
+    }
+
+    public void nextLevel(View view) {
+        setResult(NEXT_LEVEL);
         finish();
     }
 }

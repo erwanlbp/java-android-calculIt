9191f7f60048b9e32be3dbb639492f71fe987fdd
Alex <alex.vacheret@free.fr>
2017-06-29 20:46:56 +0200

    Feature/level (#6)
    * add level
    * change difficulty

---
+++ app/src/main/java/com/erwanlbp/calculit/activity/Enum/Difficulty.java
@@ -0,0 +1,40 @@
+package com.erwanlbp.calculit.activity.Enum;
+
+public enum Difficulty {
+
+    EASY(2000),
+    MEDIUM(1300),
+    HARD(800),
+    GOD(400);
+
+    private final int timeToPrint;
+
+    Difficulty(int timeToPrint) {
+        this.timeToPrint = timeToPrint;
+    }
+
+    public int getTimeToPrint() {
+        return this.timeToPrint;
+    }
+
+    public static Difficulty parse(final String difficulty) {
+        return Difficulty.valueOf(difficulty);
+    }
+
+    public static Difficulty parse(final int difficulty) {
+        switch (difficulty) {
+            case 2000:
+                return EASY;
+            case 1300:
+                return MEDIUM;
+            case 800:
+                return HARD;
+            case 400:
+                return GOD;
+            default:
+                return EASY;
+        }
+    }
+}
+
+

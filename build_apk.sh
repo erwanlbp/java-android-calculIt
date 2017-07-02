c5762b5aa9561471b704da05c1d6351368f39006
ErwanLBP <erwan.lbp@gmail.com>
2017-07-02 20:41:15 +0200

    New script to increment app version

--- build_apk.sh
+++ build_apk.sh
@@ -2,11 +2,44 @@
 
 echo
 echo "--- Build APK"
-./gradlew assemble
+gradleAssemble="$(./gradlew assemble)"
+checkBuildFailed=$(echo $gradleAssemble | grep "BUILD FAILED")
+if [ -n "$checkBuildFailed" ]; then
+	exit 1
+fi
 
-versionCode=$(grep -oP 'versionCode \K.*' ./app/build.gradle)
-versionName=$(grep -oP 'versionName "\K.*"' ./app/build.gradle | grep -o '[^"].*[^"]')
+pathToVersion="./app/build.gradle"
+
+versionCode=$(grep -oP 'versionCode \K.*' $pathToVersion)
+versionName=$(grep -oP 'versionName "\K.*"' $pathToVersion | grep -o '[^"].*[^"]')
 apkName="CalculIt-$versionCode-$versionName.apk"
+
+incAppVersion=$1
+if [ "$incAppVersion" == "incAppVersion" ]; then
+	echo 
+	echo "--- Increment App Version"
+	echo -n "New version code ? (current: $versionCode) "
+	read newVersionCode
+	sed -i "s/versionCode ${versionCode}/versionCode ${newVersionCode}/g" $pathToVersion
+
+	echo -n "New version name ? (current: $versionName) "
+	read newVersionName
+	sed -i "s/versionName \"${versionName}\"/versionName \"${newVersionName}\"/g" $pathToVersion
+
+	apkName="CalculIt-$newVersionCode-$newVersionName.apk"
+
+	echo
+	echo "--- Re-build the app"
+	gradleAssemble="$(./gradlew assemble)"
+	checkBuildFailed=$(echo $gradleAssemble | grep "BUILD FAILED")
+	if [ -n "$checkBuildFailed" ]; then
+		exit 1
+	fi
+fi
+
+echo
+echo "--- Remove previous app"
+rm -f *.apk
 
 echo
 echo "--- Move the APK to ./$apkName"
